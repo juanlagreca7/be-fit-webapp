018a01750ff91e98cc9d46cbdf3edf189efc90a9
Juan Lagreca <juanlagreca8@gmail.com>
2024-10-19 12:03:14 -0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Administradores from "./pages/Administradores";
 import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="administradores" element={<Administradores />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Administradores.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Administradores = () => {
+  return (
+    <div>
+      <h1>Administradores</h1>
+    </div>
+  );
+};
+
+export default Administradores;
